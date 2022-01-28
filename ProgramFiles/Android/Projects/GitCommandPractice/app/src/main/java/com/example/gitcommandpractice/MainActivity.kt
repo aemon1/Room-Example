e5dfe96e8bd78beaa4c65b938853aaef6f11eb3a
aemonjawaid-10p <79843444+aemonjawaid-10p@users.noreply.github.com>
2022-01-28 18:15:44 +0500

    initial commit

---
+++ ProgramFiles/Android/Projects/GitCommandPractice/app/src/main/java/com/example/gitcommandpractice/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gitcommandpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        System.out.println("Main branch")
+    }
+}
